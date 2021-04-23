b964b5576de13fa21cdd31e72bead8e984b981cc
Christopher <onlygamernerd@gmail.com>
2021-04-23 14:38:43 +0200

    fix embed

--- commands/commands.go
+++ commands/commands.go
@@ -2,15 +2,12 @@ package commands
 
 import (
 	"dizzle/config"
-	"dizzle/functions"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 func CreateCommand(s *discordgo.Session, m *discordgo.MessageCreate) {
-	functions.AvatarImage()
-
 	if strings.HasPrefix(m.Content, config.BotPrefix) {
 		if m.Author.ID == s.State.User.ID {
 			return
@@ -18,6 +15,33 @@ func CreateCommand(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 		if m.Content == "!ping" {
 			s.ChannelMessageSend(m.ChannelID, "pong!")
+		}
+
+		if m.Content == "!warn" {
+
+			warnMessage := "Warned user: " + m.Author.Mention()
+
+			s.ChannelMessageSend(m.ChannelID, warnMessage)
+		}
+
+		if m.Content == "!avatar" {
+			result := discordgo.MessageEmbed{
+				Type:        "image",
+				Title:       "Your avatar",
+				Color:       7,
+				Description: "Hello everyone",
+				Video: &discordgo.MessageEmbedVideo{
+					URL:    "https://www.youtube.com/watch?v=R85sRm01870",
+					Width:  50,
+					Height: 50,
+				},
+				Author: &discordgo.MessageEmbedAuthor{
+					Name: m.Author.Username,
+				},
+			}
+
+			s.ChannelMessageSendEmbed(m.ChannelID, &result)
+
 		}
 	}
 }
