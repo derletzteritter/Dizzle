a4d9109d54537e27c7124d487ec5155cbf1e8c8b
Christopher <onlygamernerd@gmail.com>
2021-04-23 12:56:35 +0200

    command handling

--- commands/commands.go
+++ commands/commands.go
@@ -1,7 +1,23 @@
 package commands
 
-import "fmt"
+import (
+	"dizzle/config"
+	"dizzle/functions"
+	"strings"
 
-func CreateCommand(name string) {
-	fmt.Println(name)
+	"github.com/bwmarrin/discordgo"
+)
+
+func CreateCommand(s *discordgo.Session, m *discordgo.MessageCreate) {
+	functions.AvatarImage()
+
+	if strings.HasPrefix(m.Content, config.BotPrefix) {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+
+		if m.Content == "!ping" {
+			s.ChannelMessageSend(m.ChannelID, "pong!")
+		}
+	}
 }
