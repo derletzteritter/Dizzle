49a1ea62bac1f2165f41604f9524f7299b2afb20
Christopher <onlygamernerd@gmail.com>
2021-04-23 11:10:20 +0200

    test command

--- init.go
+++ init.go
@@ -13,4 +13,16 @@ func main() {
 		fmt.Println(err)
 	}
 	fmt.Println("Started Dizzle Bot: ", discord)
+
+	discord.AddHandler(messageCreate)
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "pong!")
+	}
 }
